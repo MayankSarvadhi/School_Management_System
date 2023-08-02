e9b5d4b38f7817da78e369fa9422c1d7eedb2272
MayankSarvadhi <mayank.sarvadhi@gmail.com>
2023-08-02 17:00:36 +0530

    initial commit

---
+++ src/config/expressAppSetup.ts
@@ -0,0 +1,17 @@
+import express, { Express } from 'express';
+import * as dotenv from 'dotenv';
+import { routes } from '../routes/index';
+import { ErrorHandler } from '../middleware';
+import passport from 'passport';
+dotenv.config();
+
+export const createApp = (): Express => {
+    const app: Express = express();
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
+    app.use(passport.initialize());
+    app.use(routes);
+    app.use(ErrorHandler);
+
+    return app;
+};
