0f01800e87d7f417f381306b5c3c8a1e7d168ebd
MayankSarvadhi <mayank.sarvadhi@gmail.com>
2023-08-03 14:43:05 +0530

    initial commit

--- src/middleware/permission.Handler.ts
+++ src/middleware/permission.Handler.ts
@@ -2,10 +2,11 @@ import { AppError } from '../utils';
 
 export default function permit(roles) {
     return function (req, res, next) {
-        if (Array.isArray(roles) || req.user.Role === roles) {
+        if (Array.isArray(roles)) {
             if (roles.includes(req.user.Role)) {
                 return next();
             }
+        } else if (req.user.Role === roles) {
             return next();
         }
         throw new AppError('You do not have permission to access this route.', 'Forbidden');
